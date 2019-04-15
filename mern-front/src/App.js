b2c05b976a2c038fd86379933353cf91c1e2c898
James Kirkwood <kirkwood.jamie@gmail.com>
2019-04-15 16:47:36 -0400

    front end setup

---
+++ mern-front/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreateItem from "./components/create-item.component";
+import EditItem from "./components/edit-item.component";
+import ItemList from "./components/item-list.component";
+import logo from "./logo.svg";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="https://github.com/jkirk40" target="_blank">
+              <img src={logo} width="30" height="30" alt="react logo"/>
+            </a>
+            <Link to="/" classname="navbar-brand">Home</Link>
+            {/* <div className="collapse nav-collapse"> */}
+            <div className="nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Item List</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Item</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Route path="/" exact component={ItemList} />
+          <Route path="/edit/:id" component={EditItem} />
+          <Route path="/create" component={CreateItem} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ mern-front/src/components/create-item.component.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+
+export default class CreateItem extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            item_description: '',
+            item_responsible: '',
+            item_priority: '',
+            item_complete: false
+        }
+    }
+
+    onChangeFormText = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    onChangePriority = (e) => {
+        this.setState({
+            item_priority: e.target.value
+        });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('Form submitted');
+        console.log(`Description: ${this.state.item_description}`);
+        console.log(`Responsible: ${this.state.item_responsible}`);
+        console.log(`Priority: ${this.state.item_priority}`);
+        console.log(`Completed: ${this.state.item_complete}`);
+
+        this.setState({
+            item_description: '',
+            item_responsible: '',
+            item_priority: '',
+            item_complete: false
+        })
+    }
+
+    render() {
+        return(
+            <div style={{marginTop: 20}}>
+                <h3>Create New Item</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Description:</label>
+                        <input  type="text"
+                                name="item_description"
+                                className="form-control"
+                                value={this.state.item_description}
+                                onChange={this.onChangeFormText}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Responsible:</label>
+                        <input  type="text"
+                                name="item_responsible"
+                                className="form-control"
+                                value={this.state.item_responsible}
+                                onChange={this.onChangeFormText}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input  type="radio"
+                                    name="item_priority"
+                                    id="priority-low"
+                                    className="form-check-input"
+                                    value="Low"
+                                    checked={this.state.item_priority === 'Low'}
+                                    onChange={this.onChangePriority}
+                                    />
+                            <label className="form-check-label">Low</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input  type="radio"
+                                    name="item_priority"
+                                    id="priority-medium"
+                                    className="form-check-input"
+                                    value="Medium"
+                                    checked={this.state.item_priority === 'Medium'}
+                                    onChange={this.onChangePriority}
+                                    />
+                            <label className="form-check-label">Medium</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input  type="radio"
+                                    name="item_priority"
+                                    id="priority-high"
+                                    className="form-check-input"
+                                    value="High"
+                                    checked={this.state.item_priority === 'High'}
+                                    onChange={this.onChangePriority}
+                                    />
+                            <label className="form-check-label">High</label>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create Item" className="btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
